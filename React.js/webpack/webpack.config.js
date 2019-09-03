e861e91a1e02a9253ea3ee4c86201df497777a33
jihunhong <redgee49@gmail.com>
2019-09-04 00:25:30 +0900

    react - webpack

--- React.js/webpack/webpack.config.js
+++ React.js/webpack/webpack.config.js
@@ -1,3 +1,29 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'word-relay-setting',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: {
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: `babel-loader`,
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+            },
+        }]
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    },
 };
